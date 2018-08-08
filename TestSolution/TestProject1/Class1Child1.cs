f58a196aaccaa318e21c8cfefb97f69e77a24834
José Lima <joselima.engineering@gmail.com>
2018-08-08 19:06:23 +0200

    Adds a test solution.

---
+++ TestSolution/TestProject1/Class1Child1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1 {
+
+    public class Class1Child1: Class1 {
+    }
+}
